3c2a45c42d1a2b3ddb4260ddb42d6722961006ac
chenshan01 <chenfenping@emoney.con>
2014-12-04 15:02:46 +0800

    for-loop & while-loop

---
+++ 33.py
@@ -0,0 +1,15 @@
+#------- for-loop ------
+number=[]
+
+for i in range(0,6):
+
+    number.append(i)
+    print("The nunmber:", number)
+
+#------- while-loop ------
+i=0
+x=[]
+while i <3:
+    x.append(i)
+    i+=1
+    print("The x:",x)
